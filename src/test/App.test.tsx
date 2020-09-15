cf6bce4649ecc10008830a9344e1db89264cfd74
John Martin Lindseth <john.martin.lindseth@nav.no>
2020-09-15 21:24:46 +0200

    Convert to typescript

--- src/test/App.test.tsx
+++ src/test/App.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import App from '../src/components/App';
+import '@testing-library/jest-dom/extend-expect';
+import App from '../components/App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
